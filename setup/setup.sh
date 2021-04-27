f63e853227acc29986112f866f9a0bc84ab87c46
Jonathan Locke <jonathanl@telenav.com>
2021-04-27 09:15:57 -0700

    🐛 Script fixes

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,22 +7,50 @@
 #
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-if [ -z "$KIVAKIT_HOME" ]; then
+if [ -z "$KIVAKIT_WORKSPACE" ]; then
     echo "You must set up your environment to continue setting up KivaKit."
     echo "See http://kivakit.org for details."
     exit 1
 fi
+
+#
+# Check out related repositories
+#
 
 cd $KIVAKIT_WORKSPACE
 git clone https://github.com/Telenav/kivakit-data.git
 git clone https://github.com/Telenav/kivakit-build.git
 git clone https://github.com/Telenav/kivakit-extensions.git
 
+#
+# Initialize git flow
+#
+
+cd $KIVAKIT_WORKSPACE
+git flow init -d
+git checkout develop
+cd $KIVAKIT_WORKSPACE/kivakit-data
+git flow init -d
+git checkout develop
+cd $KIVAKIT_WORKSPACE/kivakit-build
+git flow init -d
+git checkout develop
+cd $KIVAKIT_WORKSPACE/kivakit-extensions
+git flow init -d
+git checkout develop
+
+#
+# Check out Lexakai annotations (required to build KivaKit)
+#
+
 cd $KIVAKIT_WORKSPACE
 git clone https://github.com/Telenav/lexakai-annotations.git
 cd lexakai-annotations
 mvn clean install
 
+#
+# Build
+#
+
 cd $KIVAKIT_HOME
-git checkout develop
 kivakit-build.sh all clean
