2137eaaac8d297877c2963a0327030cd5380ab85
jonathan <jon@thanlocke.com>
2021-05-11 10:57:10 -0700

    👷 fix some build issues

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,38 +14,46 @@ if [ -z "$KIVAKIT_WORKSPACE" ]; then
 fi
 
 #
-# Check out related repositories
+# Check out required repositories
 #
 
 cd $KIVAKIT_WORKSPACE
-git clone https://github.com/Telenav/kivakit-data.git
 git clone https://github.com/Telenav/kivakit-build.git
+git clone https://github.com/Telenav/lexakai-annotations.git
+git clone https://github.com/Telenav/kivakit-data.git
 git clone https://github.com/Telenav/kivakit-extensions.git
 
 #
-# Initialize git flow
+# Initialize git flow for each project
 #
 
 cd $KIVAKIT_WORKSPACE
 git flow init -d
 git checkout develop
-cd $KIVAKIT_WORKSPACE/kivakit-data
-git flow init -d
-git checkout develop
+
 cd $KIVAKIT_WORKSPACE/kivakit-build
 git flow init -d
 git checkout develop
+
+cd $KIVAKIT_WORKSPACE/lexakai-annotations
+cd lexakai-annotations
+git flow init -d
+
+cd $KIVAKIT_WORKSPACE/kivakit-data
+git flow init -d
+git checkout develop
+
 cd $KIVAKIT_WORKSPACE/kivakit-extensions
 git flow init -d
 git checkout develop
 
 #
-# Check out Lexakai annotations (required to build KivaKit)
+# Build kivakit-build and lexakai-annotations (required to build all projects)
 #
 
-cd $KIVAKIT_WORKSPACE
-git clone https://github.com/Telenav/lexakai-annotations.git
-cd lexakai-annotations
+cd $KIVAKIT_WORKSPACE/kivakit-build
+mvn clean install
+cd $KIVAKIT_WORKSPACE/lexakai-annotations
 mvn clean install
 
 #
