328a1102008939f18c860c7d2a9e5e08e6574587
haifeng-zhu-tn <hfzhu@telenav.com>
2021-05-13 15:47:41 -0700

    merge changes on develop

--- setup/setup.sh
+++ setup/setup.sh
@@ -14,38 +14,47 @@ if [ -z "$KIVAKIT_WORKSPACE" ]; then
 fi
 
 #
-# Check out related repositories
+# Check out required repositories
 #
 
 cd $KIVAKIT_WORKSPACE
-git clone https://github.com/Telenav/kivakit-assets.git
 git clone https://github.com/Telenav/cactus-build.git
+git clone https://github.com/Telenav/kivakit-assets.git
+git clone https://github.com/Telenav/lexakai-annotations.git
 git clone https://github.com/Telenav/kivakit-extensions.git
 
 #
-# Initialize git flow
+# Initialize git flow for each project
 #
 
-cd $KIVAKIT_WORKSPACE
+cd $KIVAKIT_WORKSPACE/kivakit
 git flow init -d
 git checkout develop
-cd $KIVAKIT_WORKSPACE/kivakit-assets
-git flow init -d
-git checkout develop
+
 cd $KIVAKIT_WORKSPACE/cactus-build
 git flow init -d
 git checkout develop
+
+cd $KIVAKIT_WORKSPACE/kivakit-assets
+git flow init -d
+git checkout develop
+
+cd $KIVAKIT_WORKSPACE/lexakai-annotations
+cd lexakai-annotations
+git flow init -d
+
 cd $KIVAKIT_WORKSPACE/kivakit-extensions
 git flow init -d
 git checkout develop
 
 #
-# Check out Lexakai annotations (required to build KivaKit)
+# Build cactus-build and lexakai-annotations (required to build all projects)
 #
 
-cd $KIVAKIT_WORKSPACE
-git clone https://github.com/Telenav/lexakai-annotations.git
-cd lexakai-annotations
+cd $KIVAKIT_WORKSPACE/cactus-build
+mvn clean install
+
+cd $KIVAKIT_WORKSPACE/lexakai-annotations
 mvn clean install
 
 #
