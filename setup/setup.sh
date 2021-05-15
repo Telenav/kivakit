48739cfee517fa052f6ae2b4a2b91b5f1542f23b
jonathan <jon@thanlocke.com>
2021-05-15 13:56:06 -0700

    👷 Revise shell restart check

--- setup/setup.sh
+++ setup/setup.sh
@@ -15,43 +15,8 @@ if [ -z "$KIVAKIT_WORKSPACE" ]; then
     exit 1
 fi
 
-#
-# Check out required repositories
-#
-
-cd $KIVAKIT_WORKSPACE
-git clone https://github.com/Telenav/cactus-build.git
-git clone https://github.com/Telenav/kivakit-assets.git
-git clone https://github.com/Telenav/lexakai-annotations.git
-git clone https://github.com/Telenav/kivakit-extensions.git
-
-#
-# Initialize git flow for each project
-#
-
 cd $KIVAKIT_WORKSPACE/kivakit
-git flow init -d
-git checkout develop
-
-cd $KIVAKIT_WORKSPACE/cactus-build
-git flow init -d
-git checkout develop
-
-cd $KIVAKIT_WORKSPACE/kivakit-assets
-git flow init -d
-git checkout develop
-
-cd $KIVAKIT_WORKSPACE/lexakai-annotations
-cd lexakai-annotations
-git flow init -d
-
-cd $KIVAKIT_WORKSPACE/kivakit-extensions
-git flow init -d
-git checkout develop
-
-#
-# Build
-#
+git checkout -q develop
 
 if [ -z "$CACTUS_HOME" ]; then
     echo " "
@@ -60,6 +25,42 @@ if [ -z "$CACTUS_HOME" ]; then
     echo " "
     exit 1
 fi
+
+#
+# Check out required repositories
+#
+
+cd $KIVAKIT_WORKSPACE
+git clone -q https://github.com/Telenav/cactus-build.git
+git clone -q https://github.com/Telenav/kivakit-assets.git
+git clone -q https://github.com/Telenav/lexakai-annotations.git
+git clone -q https://github.com/Telenav/kivakit-extensions.git
+
+#
+# Initialize git flow for each project
+#
+
+cd $KIVAKIT_WORKSPACE/cactus-build
+git flow init -f -d
+
+cd $KIVAKIT_WORKSPACE/cactus-build-assets
+git flow init -f -d
+
+cd $KIVAKIT_WORKSPACE/lexakai-annotations
+git flow init -f -d
+
+cd $KIVAKIT_WORKSPACE/kivakit
+git flow init -f -d
+
+cd $KIVAKIT_WORKSPACE/kivakit-assets
+git flow init -f -d
+
+cd $KIVAKIT_WORKSPACE/kivakit-extensions
+git flow init -f -d
+
+#
+# Build
+#
 
 cd $KIVAKIT_HOME
 kivakit-build.sh setup
