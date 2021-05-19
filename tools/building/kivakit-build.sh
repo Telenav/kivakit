9be6b8b390fd55d0d2049916fae98c973d6f6f8a
jonathan <jon@thanlocke.com>
2021-05-18 19:28:49 -0700

    👷 Download cactus-build to temporary folder, remove from assets folders  - Stop building cactus-build and lexakai-annotations as part of kivakit build

--- tools/building/kivakit-build.sh
+++ tools/building/kivakit-build.sh
@@ -11,9 +11,6 @@ source library-functions.sh
 source library-build.sh
 source kivakit-projects.sh
 
-build $KIVAKIT_WORKSPACE/cactus-build $@
-build $KIVAKIT_WORKSPACE/lexakai-annotations $@
-
 for project_home in "${KIVAKIT_PROJECT_HOMES[@]}"; do
 
     build $project_home $@
