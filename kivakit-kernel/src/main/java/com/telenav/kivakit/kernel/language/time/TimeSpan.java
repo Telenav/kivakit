02bb1c249fb6a7b153e12917cb5075f8ec17697f
jonathan <jon@thanlocke.com>
2021-07-26 16:39:07 -0700

    🎨 Add timespan queries

--- kivakit-kernel/src/main/java/com/telenav/kivakit/kernel/language/time/TimeSpan.java
+++ kivakit-kernel/src/main/java/com/telenav/kivakit/kernel/language/time/TimeSpan.java
@@ -2,6 +2,11 @@ package com.telenav.kivakit.kernel.language.time;
 
 public class TimeSpan
 {
+    public static TimeSpan all()
+    {
+        return new TimeSpan(Time.START_OF_UNIX_TIME, Time.MAXIMUM);
+    }
+
     public static TimeSpan of(final Time start, final Time end)
     {
         return new TimeSpan(start, end);
@@ -16,10 +21,18 @@ public class TimeSpan
 
     private final Time end;
 
-    public TimeSpan(final Time start, final Time end)
+    protected TimeSpan(final Time start, final Time end)
     {
         this.start = start;
         this.end = end;
+    }
+
+    /**
+     * @return True if this time span contains the given time (endpoints are inclusive)
+     */
+    public boolean contains(final Time time)
+    {
+        return time.isAtOrAfter(start) && time.isAtOrBefore(end);
     }
 
     public Duration duration()
