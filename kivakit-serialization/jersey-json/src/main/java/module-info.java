d077e0811aa27f0db7e9d7e91adfe73415048bbe
jonathan <jon@thanlocke.com>
2021-09-01 14:54:54 -0700

    🚧 Work in progress on microservices

--- kivakit-serialization/jersey-json/src/main/java/module-info.java
+++ kivakit-serialization/jersey-json/src/main/java/module-info.java
@@ -3,8 +3,9 @@ open module kivakit.serialization.jersey.json
     // KivaKit
     requires transitive kivakit.serialization.json;
 
-    // Jersey bindings
+    // Jersey
     requires transitive java.ws.rs;
+    requires transitive java.xml.bind;
 
     // Module exports
     exports com.telenav.kivakit.serialization.jersey.json;
