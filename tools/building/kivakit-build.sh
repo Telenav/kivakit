622f091b8e3d6e89b5d8f46f771b63da9c139990
jonathan <jon@thanlocke.com>
2021-09-03 18:20:22 -0700

    👷 Switch all projects to hierarchical superpom structure

--- tools/building/kivakit-build.sh
+++ tools/building/kivakit-build.sh
@@ -11,8 +11,15 @@ source library-functions.sh
 source library-build.sh
 source kivakit-projects.sh
 
+cd "$KIVAKIT_HOME"/superpom
+mvn clean install
+
+export ALLOW_CLEANING=true
+
 for project_home in "${KIVAKIT_PROJECT_HOMES[@]}"; do
 
     build "$project_home" "$@"
+
+    export ALLOW_CLEANING=false
 
 done
