42dfb838dce973c41eca3e66f1fd8d58e8b60a60
jonathan <jon@thanlocke.com>
2021-09-04 20:54:56 -0700

    👷 Fix deployment scripts

---
+++ .github/scripts/build.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#
+#  © 2011-2021 Telenav, Inc.
+#  Licensed under Apache License, Version 2.0
+#
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use strict;
+use warnings FATAL => 'all';
+
+#
+# Include build script from cactus-build
+#
+
+system("git clone --branch develop --quiet https://github.com/Telenav/cactus-build.git");
+
+require "./cactus-build/.github/scripts/build-include.pl";
+
+#
+# Get build type and branch
+#
+
+my ($build_type, $reference) = @ARGV;
+my $branch = reference_to_branch($reference);
+
+check_build_type($build_type);
+check_branch($branch);
+
+say("Building $branch ($build_type)");
+
+#
+# Clone repositories
+#
+
+my $github = "https://github.com/Telenav";
+
+clone("$github/kivakit", $branch);
+
+#
+# Build repositories
+#
+
+build_kivakit($build_type);
+
