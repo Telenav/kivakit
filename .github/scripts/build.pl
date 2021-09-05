0e2404e07646573bf4be54174ba9ac00976ddde3
jonathan <jon@thanlocke.com>
2021-09-05 13:28:22 -0700

    👷 Update build script

--- .github/scripts/build.pl
+++ .github/scripts/build.pl
@@ -14,9 +14,13 @@ use warnings FATAL => 'all';
 # Include build script from cactus-build
 #
 
-system("git clone --branch develop --quiet https://github.com/Telenav/cactus-build.git");
+if (!-d "cactus-build")
+{
+    system("git clone --branch develop --quiet https://github.com/Telenav/cactus-build.git");
+}
 
 require "./cactus-build/.github/scripts/build-include.pl";
+#require "$ENV{'KIVAKIT_WORKSPACE'}/cactus-build/.github/scripts/build-include.pl";
 
 #
 # Get build type and branch
