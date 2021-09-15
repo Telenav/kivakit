dc2f951006b9abd77f9733a89c22f34cf2ac9ad2
jonathan <jon@thanlocke.com>
2021-09-14 19:13:29 -0700

    📝 Improve Json and Swagger models

--- kivakit-serialization/json/src/main/java/com/telenav/kivakit/serialization/json/serializers/UtcTimeInKivaKitFormatGsonSerializer.java
+++ kivakit-serialization/json/src/main/java/com/telenav/kivakit/serialization/json/serializers/UtcTimeInKivaKitFormatGsonSerializer.java
@@ -19,6 +19,7 @@
 package com.telenav.kivakit.serialization.json.serializers;
 
 import com.telenav.kivakit.kernel.language.time.LocalTime;
+import com.telenav.kivakit.kernel.language.time.Time;
 import com.telenav.kivakit.kernel.language.time.conversion.converters.LocalDateTimeConverter;
 import com.telenav.kivakit.kernel.logging.Logger;
 import com.telenav.kivakit.kernel.logging.LoggerFactory;
@@ -26,29 +27,29 @@ import com.telenav.kivakit.serialization.json.PrimitiveGsonSerializer;
 import com.telenav.lexakai.annotations.LexakaiJavadoc;
 
 /**
- * Serializes {@link LocalTime} objects to and from JSON in KivaKit format.
+ * Serializes {@link Time} objects to and from JSON as a number of milliseconds since the start of the UNIX epoch.
  *
  * @author jonathanl (shibo)
  */
 @LexakaiJavadoc(complete = true)
-public class LocalTimeGsonSerializer extends PrimitiveGsonSerializer<LocalTime, String>
+public class UtcTimeInKivaKitFormatGsonSerializer extends PrimitiveGsonSerializer<Time, String>
 {
     private static final Logger LOGGER = LoggerFactory.newLogger();
 
-    public LocalTimeGsonSerializer()
+    public UtcTimeInKivaKitFormatGsonSerializer()
     {
         super(String.class);
     }
 
     @Override
-    protected LocalTime toObject(final String text)
+    protected Time toObject(final String identifier)
     {
-        return new LocalDateTimeConverter(LOGGER, LocalTime.localTimeZone()).convert(text);
+        return new LocalDateTimeConverter(LOGGER, LocalTime.utcTimeZone()).convert(identifier);
     }
 
     @Override
-    protected String toPrimitive(final LocalTime time)
+    protected String toPrimitive(final Time time)
     {
-        return time.utc().toString();
+        return time.localTime(LocalTime.utcTimeZone()).asDateTimeString();
     }
 }
