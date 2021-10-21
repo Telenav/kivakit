5d90e7ab833de17ef88e0d6125636ef9ef4b0a8d
jonathan <jon@thanlocke.com>
2021-10-21 13:30:37 -0700

    💚 Debugging install script  - Improve concurrency issue in statistics aggregation

--- tools/library/install-merged-jars.sh
+++ tools/library/install-merged-jars.sh
@@ -4,7 +4,7 @@ if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-grpc-merged/1.1.0/
 
     echo "Installing GRPC merged JAR"
 
-    mvn --quiet install:install-file \
+    mvn install:install-file \
         -DgroupId=com.telenav.kivakit \
         -DartifactId=kivakit-grpc-merged \
         -Dfile="${KIVAKIT_EXTENSIONS_HOME}"/kivakit-merged-jars/lib/kivakit-grpc-merged-1.1.0.jar \
@@ -17,7 +17,7 @@ if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-protostuff-merged/
 
     echo "Installing Protostuff merged JAR"
 
-    mvn --quiet install:install-file \
+    mvn install:install-file \
         -DgroupId=com.telenav.kivakit \
         -DartifactId=kivakit-protostuff-merged \
         -Dfile="${KIVAKIT_EXTENSIONS_HOME}"/kivakit-merged-jars/lib/kivakit-protostuff-merged-1.1.0.jar \
