18c8359429b5ea2b840e762f7e34277c08ad8aa7
jonathan <jon@thanlocke.com>
2021-11-29 13:27:16 -0700

    🎨 Reshaped merged jars

--- tools/library/install-merged-jars.sh
+++ tools/library/install-merged-jars.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged-jars/"
+cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged/"
 
-if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-grpc-merged/${KIVAKIT_VERSION}/kivakit-grpc-merged-${KIVAKIT-VERSION}.jar" ]; then
+if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-grpc/${KIVAKIT_VERSION}/kivakit-merged-grpc-${KIVAKIT-VERSION}.jar" ]; then
 
     echo "Installing GRPC merged JAR"
 
-    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged-jars/kivakit-grpc-merged"
+    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged/kivakit-merged-grpc"
     mvn --batch-mode clean install
 
 fi
 
-if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-protostuff-merged/${KIVAKIT_VERSION}/kivakit-protostuff-merged-${KIVAKIT_VERSION}.jar" ]; then
+if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-protostuff/${KIVAKIT_VERSION}/kivakit-merged-protostuff-${KIVAKIT_VERSION}.jar" ]; then
 
     echo "Installing Protostuff merged JAR"
 
-    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged-jars/kivakit-protostuff-merged"
+    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged/kivakit-merged-protostuff"
     mvn --batch-mode clean install
 
 fi
 
-if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-prometheus-merged/${KIVAKIT_VERSION}/kivakit-prometheus-merged-${KIVAKIT_VERSION}.jar" ]; then
+if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-prometheus/${KIVAKIT_VERSION}/kivakit-merged-prometheus-${KIVAKIT_VERSION}.jar" ]; then
 
     echo "Installing Prometheus merged JAR"
 
-    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged-jars/kivakit-prometheus-merged"
+    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged/kivakit-merged-prometheus"
+    mvn --batch-mode clean install
+
+fi
+
+if [ ! -e "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-zookeeper/${KIVAKIT_VERSION}/kivakit-merged-zookeeper-${KIVAKIT_VERSION}.jar" ]; then
+
+    echo "Installing Zookeeper merged JAR"
+
+    cd "${KIVAKIT_EXTENSIONS_HOME}/kivakit-merged/kivakit-merged-zookeeper"
     mvn --batch-mode clean install
 
 fi
