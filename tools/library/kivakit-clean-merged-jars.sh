1e9500762e98562c2be8971aeef55e59e4241761
jonathan <jon@thanlocke.com>
2021-12-06 18:35:52 -0700

    🔨 Fix merged jars scripts  - Add documentation for ZookeeperConnection  - Fix bug in ZookeeperSettingsStoreTest

---
+++ tools/library/kivakit-clean-merged-jars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged"
+rm -rf "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-grpc"
+rm -rf "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-protostuff"
+rm -rf "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-prometheus"
+rm -rf "${HOME}/.m2/repository/com/telenav/kivakit/kivakit-merged-zookeeper"
+
