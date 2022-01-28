a295d0e16e22b931212cf408b526aa7992bafb72
Jonathan Locke <jonathanl@telenav.com>
2022-01-27 19:28:06 -0700

    👷 Clone only one revision of assets repositories

--- setup/setup-repositories.sh
+++ setup/setup-repositories.sh
@@ -23,7 +23,7 @@ git clone https://github.com/Telenav/cactus-build.git
 
 git clone https://github.com/Telenav/lexakai-annotations.git
 
-git clone https://github.com/Telenav/kivakit-assets.git
+git clone --depth 1 https://github.com/Telenav/kivakit-assets.git
 git clone https://github.com/Telenav/kivakit-extensions.git
 git clone https://github.com/Telenav/kivakit-examples.git
 
