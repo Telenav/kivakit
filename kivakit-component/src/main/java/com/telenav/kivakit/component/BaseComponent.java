5e12a5a1a7ac5ae077b7edfe8ca50bb8979052a9
jonathan <jon@thanlocke.com>
2022-01-30 16:51:49 -0700

    🎨 Formatting

--- kivakit-component/src/main/java/com/telenav/kivakit/component/BaseComponent.java
+++ kivakit-component/src/main/java/com/telenav/kivakit/component/BaseComponent.java
@@ -4,13 +4,10 @@ import com.telenav.kivakit.configuration.lookup.Registry;
 import com.telenav.kivakit.configuration.lookup.RegistryTrait;
 import com.telenav.kivakit.configuration.settings.Settings;
 import com.telenav.kivakit.configuration.settings.SettingsTrait;
-import com.telenav.kivakit.kernel.language.strings.formatting.ObjectFormatter;
 import com.telenav.kivakit.kernel.messaging.Message;
 import com.telenav.kivakit.kernel.messaging.repeaters.BaseRepeater;
 
 import java.util.function.Consumer;
-
-import static com.telenav.kivakit.kernel.language.strings.formatting.ObjectFormatter.Format.MULTILINE;
 
 /**
  * Base class for KivaKit components. Provides easy access to object registration and lookup (see {@link Registry}) as
@@ -21,7 +18,10 @@ import static com.telenav.kivakit.kernel.language.strings.formatting.ObjectForma
  * @see Registry
  * @see Settings
  */
-public class BaseComponent extends BaseRepeater implements Component, RegistryTrait, SettingsTrait
+public class BaseComponent extends BaseRepeater implements
+        Component,
+        RegistryTrait,
+        SettingsTrait
 {
     /**
      * Call the consumer with any messages this component hears
