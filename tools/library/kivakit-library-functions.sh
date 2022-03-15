dc778dea413f66cdcf1ae7e6b93b4b164cde9ad9
jonathan <jon@thanlocke.com>
2022-03-15 08:23:46 -0600

    🔖 prepare for 1.4 release

--- tools/library/kivakit-library-functions.sh
+++ tools/library/kivakit-library-functions.sh
@@ -226,7 +226,7 @@ git_flow_release_start() {
     git checkout release/"$version"
 
     # and update its version
-    bash kivakit-release-update-version.sh "$version"
+    bash update_version "$project_home" "$version"
 }
 
 git_branch_name()
