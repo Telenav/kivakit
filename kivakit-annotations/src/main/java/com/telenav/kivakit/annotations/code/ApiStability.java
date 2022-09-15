c3cbb837fc14a5cd5abb04388a52bd5ac3e5da4f
jonathanl-telenav <jonathanl@telenav.com>
2022-09-15 17:06:22 -0600

    📝 add code quality annotation

---
+++ kivakit-annotations/src/main/java/com/telenav/kivakit/annotations/code/ApiStability.java
@@ -0,0 +1,15 @@
+package com.telenav.kivakit.annotations.code;
+
+/**
+ * The level of API stability for a class, as evaluated by a developer. This is different from a measure of past source
+ * code change because it is future-looking. It is based on the <i>anticipated</i> level of change in the future.
+ *
+ * @author jonathanl (shibo)
+ */
+@SuppressWarnings("unused")
+public enum ApiStability
+{
+    STABLE,
+    UNSTABLE,
+    UNEVALUATED
+}
---
+++ kivakit-annotations/src/main/java/com/telenav/kivakit/annotations/code/DocumentationQuality.java
@@ -0,0 +1,14 @@
+package com.telenav.kivakit.annotations.code;
+
+/**
+ * An evaluation of the quality of documentation for this class, as determined by a developer. Some classes need more
+ * documentation, some less, so subjective opinion is necessary to determine documentation quality level.
+ *
+ * @author jonathanl (shibo)
+ */
+public enum DocumentationQuality
+{
+    COMPLETE,
+    INCOMPLETE,
+    UNEVALUATED
+}
