a47fb09191a665446ce66dbf3426805db7e6ee4b
jonathanl-telenav <jonathanl@telenav.com>
2022-09-27 11:06:25 -0600

    🚧 finished evaluation of locale/iso code package

--- kivakit-core/src/main/java/com/telenav/kivakit/core/locale/Locale.java
+++ kivakit-core/src/main/java/com/telenav/kivakit/core/locale/Locale.java
@@ -18,84 +18,99 @@
 
 package com.telenav.kivakit.core.locale;
 
+import com.telenav.kivakit.core.collections.list.ObjectList;
+import com.telenav.kivakit.core.internal.lexakai.DiagramLocale;
 import com.telenav.kivakit.core.language.Objects;
 import com.telenav.kivakit.core.path.StringPath;
-import com.telenav.kivakit.core.internal.lexakai.DiagramLocale;
+import com.telenav.kivakit.core.string.StringTo;
 import com.telenav.lexakai.annotations.UmlClassDiagram;
 import com.telenav.lexakai.annotations.associations.UmlAggregation;
 
+import java.util.Collection;
+
+import static com.telenav.kivakit.core.collections.list.ObjectList.objectList;
+
 /**
- * A locale constructed from a {@link LanguageIsoCode} and an optional {@link CountryIsoCode}. Provides a unique path to
- * localized resources for this locale with {@link #path()}.
+ * A locale constructed from a {@link LocaleLanguage} and an optional {@link LocaleCountry}. Provides a unique path to
+ * localized resources for this locale with {@link #path(String)}.
  *
  * @author jonathanl (shibo)
  */
+@SuppressWarnings("unused")
 @UmlClassDiagram(diagram = DiagramLocale.class)
 public class Locale
 {
-    /** The language spoken in this locale */
-    @UmlAggregation
-    private final LanguageIsoCode language;
-
     /** The country for the locale, which might increase specificity, for example, US and Australian English. */
     @UmlAggregation
-    private CountryIsoCode country;
+    private final LocaleCountry country;
+
+    /** ISO codes for the languages spoken in this locale */
+    @UmlAggregation
+    private final ObjectList<LocaleLanguage> languages;
 
     /**
-     * @param language The language spoken in this locale
      * @param country The country
+     * @param languages The languages spoken in this locale
      */
-    public Locale(LanguageIsoCode language, CountryIsoCode country)
+    public Locale(LocaleCountry country, Collection<LocaleLanguage> languages)
     {
-        this.language = language;
         this.country = country;
+        this.languages = objectList(languages);
     }
 
     /**
-     * @param language The language for this locale throughout the world, for example, World English
+     * @param languages The language for this locale throughout the world, for example, World English
      */
-    public Locale(LanguageIsoCode language)
+    public Locale(Collection<LocaleLanguage> languages)
     {
-        this.language = language;
+        this.country = null;
+        this.languages = objectList(languages);
     }
 
     /**
+     * @param name The name of the given language within this locale
      * @return A Java {@link java.util.Locale} for this KivaKit {@link Locale}
      */
-    public java.util.Locale asJavaLocale()
+    public java.util.Locale asJavaLocale(String name)
     {
         return country == null
-                ? new java.util.Locale(language.iso3Code())
-                : new java.util.Locale(language.iso3Code(), country.alpha2Code());
+                ? new java.util.Locale(language(name).iso3Code())
+                : new java.util.Locale(language(name).iso3Code(), country.alpha2Code());
     }
 
     /**
      * @return Any country for this locale, or null if the locale is relevant to the whole world
      */
-    public CountryIsoCode country()
+    public LocaleCountry country()
     {
         return country;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object object)
     {
         if (object instanceof Locale)
         {
             var that = (Locale) object;
-            return Objects.isEqual(language, that.language);
+            return Objects.isEqual(languages, that.languages);
         }
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode()
     {
-        return language().hashCode();
+        return languages().hashCode();
     }
 
     /**
-     * @return True if this is a world locale
+     * @return True if this is the world locale
      */
     public boolean isWorld()
     {
@@ -103,29 +118,48 @@ public class Locale
     }
 
     /**
-     * @return The language for this locale
+     * The language ISO codes for the given language
+     *
+     * @param name The name of the language
+     * @return The ISO codes
      */
-    public LanguageIsoCode language()
+    public LocaleLanguage language(String name)
     {
-        return language;
+        for (var language : languages())
+        {
+            if (language.name().equalsIgnoreCase(name))
+            {
+                return language;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @return The languages spoken in this locale
+     */
+    public ObjectList<LocaleLanguage> languages()
+    {
+        return languages;
     }
 
     /**
      * Produces a path of the form "locales/[language-name](/[country-name])?. This path can be used when loading
-     * localizable resources.
+     * localized resources.
      *
+     * @param name The name of the language
      * @return A relative path of the given type for this locale
      */
-    public StringPath path()
+    public StringPath path(String name)
     {
         return country == null
-                ? StringPath.stringPath("locales", language.name())
-                : StringPath.stringPath("locales", language.name(), country.name());
+                ? StringPath.stringPath("locales", name)
+                : StringPath.stringPath("locales", name, country.name());
     }
 
     @Override
     public String toString()
     {
-        return language().name() + (country == null ? "" : "." + country.name());
+        return "[country = " + StringTo.nonNullString(country.name()) + ": " + languages().join(", ") + "]";
     }
 }
