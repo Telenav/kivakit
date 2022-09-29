d4c6f60698cd3319f97611ebc8e3cc35286aae98
jonathanl-telenav <jonathanl@telenav.com>
2022-09-29 11:14:51 -0600

    🚧 completed analysis of kivakit-component project

--- kivakit-component/src/main/java/com/telenav/kivakit/component/BaseComponent.java
+++ kivakit-component/src/main/java/com/telenav/kivakit/component/BaseComponent.java
@@ -1,10 +1,15 @@
 package com.telenav.kivakit.component;
 
+import com.telenav.kivakit.annotations.code.ApiQuality;
 import com.telenav.kivakit.component.internal.lexakai.DiagramComponent;
 import com.telenav.kivakit.core.messaging.Repeater;
 import com.telenav.kivakit.core.messaging.repeaters.BaseRepeater;
 import com.telenav.kivakit.interfaces.naming.NamedObject;
 import com.telenav.lexakai.annotations.UmlClassDiagram;
+
+import static com.telenav.kivakit.annotations.code.ApiStability.STABLE_EXPANDABLE;
+import static com.telenav.kivakit.annotations.code.DocumentationQuality.FULLY_DOCUMENTED;
+import static com.telenav.kivakit.annotations.code.TestingQuality.UNTESTED;
 
 /**
  * Base class for KivaKit components. For details, see {@link Component}.
@@ -14,17 +19,28 @@ import com.telenav.lexakai.annotations.UmlClassDiagram;
  * @see Repeater
  */
 @UmlClassDiagram(diagram = DiagramComponent.class)
+@ApiQuality(stability = STABLE_EXPANDABLE,
+            testing = UNTESTED,
+            documentation = FULLY_DOCUMENTED)
 public class BaseComponent extends BaseRepeater implements Component
 {
     /** The name of this object for debugging purposes */
     private String objectName = NamedObject.syntheticName(this);
 
+    /**
+     * Sets the object name for this component
+     *
+     * @param objectName The object's new name
+     */
     @Override
     public void objectName(String objectName)
     {
         this.objectName = objectName;
     }
 
+    /**
+     * Returns this component's object name
+     */
     @Override
     public String objectName()
     {
--- kivakit-component/src/main/java/com/telenav/kivakit/component/ComponentMixin.java
+++ kivakit-component/src/main/java/com/telenav/kivakit/component/ComponentMixin.java
@@ -1,10 +1,15 @@
 package com.telenav.kivakit.component;
 
+import com.telenav.kivakit.annotations.code.ApiQuality;
 import com.telenav.kivakit.component.internal.lexakai.DiagramComponent;
 import com.telenav.kivakit.core.messaging.Repeater;
 import com.telenav.kivakit.core.messaging.repeaters.RepeaterMixin;
 import com.telenav.kivakit.mixins.Mixin;
 import com.telenav.lexakai.annotations.UmlClassDiagram;
+
+import static com.telenav.kivakit.annotations.code.ApiStability.STABLE_EXPANDABLE;
+import static com.telenav.kivakit.annotations.code.DocumentationQuality.FULLY_DOCUMENTED;
+import static com.telenav.kivakit.annotations.code.TestingQuality.UNTESTED;
 
 /**
  * A mixin for {@link Component} which can be implemented by a class that already extends another base class.
@@ -15,7 +20,13 @@ import com.telenav.lexakai.annotations.UmlClassDiagram;
  * @see RepeaterMixin
  */
 @UmlClassDiagram(diagram = DiagramComponent.class)
-public interface ComponentMixin extends Component, Mixin, RepeaterMixin
+@ApiQuality(stability = STABLE_EXPANDABLE,
+            testing = UNTESTED,
+            documentation = FULLY_DOCUMENTED)
+public interface ComponentMixin extends
+        Component,
+        Mixin,
+        RepeaterMixin
 {
     /**
      * @return The {@link BaseComponent} implementation associated with this mixin
@@ -25,18 +36,27 @@ public interface ComponentMixin extends Component, Mixin, RepeaterMixin
         return mixin(ComponentMixin.class, BaseComponent::new);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     default String objectName()
     {
         return component().objectName();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     default void objectName(String objectName)
     {
         component().objectName(objectName);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     default Repeater repeater()
     {
