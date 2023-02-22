3d56140af5dfbdb73b72bf33761d0ad06573bb40
jonathanl-telenav <jonathanl@telenav.com>
2023-02-22 09:58:18 -0700

    ✨ added applyTo() overloads to Functions

--- kivakit-internal/tests/core/src/test/java/com/telenav/kivakit/internal/tests/core/function/FunctionsTest.java
+++ kivakit-internal/tests/core/src/test/java/com/telenav/kivakit/internal/tests/core/function/FunctionsTest.java
@@ -7,21 +7,31 @@ import static com.telenav.kivakit.core.function.Functions.applyTo;
 
 public class FunctionsTest
 {
-    private record Street(String name) {}
     private record Address(Street street) {}
+
+    private record Street(String name) {}
+
     private record User(Address address) {}
+
+    @Test
+    public void testApplyTo()
+    {
+        var name =
+            applyTo(user(), user ->
+                applyTo(user.address(), address ->
+                    applyTo(address.street(), Street::name)));
+        ensureEqual(name, "Shibo Street");
+    }
+
+    @Test
+    public void testApplyToMultiple()
+    {
+        var name = applyTo(user(), User::address, Address::street, Street::name);
+        ensureEqual(name, "Shibo Street");
+    }
 
     private User user()
     {
         return new User(new Address(new Street("Shibo Street")));
-    }
-
-    @Test
-    public void test()
-    {
-        var name = applyTo(user(), user ->
-            applyTo(user.address(), address ->
-                applyTo(address.street(), Street::name)));
-        ensureEqual(name, "Shibo Street");
     }
 }
